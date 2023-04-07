559a107ed0c12e3c33c564bf486dde3fc8dd34ea
Jeremy Jordan <jjordan99@gmail.com>
2023-04-07 17:18:56 -0500

    Remove sample weather code from template

--- replayParser/Program.cs
+++ replayParser/Program.cs
@@ -18,26 +18,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
-
 app.MapPost("/parseFile", async (IFormFile file) => {
 
     Fraxiinus.Rofl.Extract.Data.Models.ROFL replay;
@@ -54,8 +34,3 @@ app.MapPost("/parseFile", async (IFormFile file) => {
 });
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
